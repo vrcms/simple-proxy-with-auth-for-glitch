134a53632f9be81b120a3dca6868e73f5eb990b0
Jason.lu <vrcms@users.noreply.github.com>
2021-07-28 10:49:07 +0800

    Update node-proxy.js

--- api/node-proxy.js
+++ api/node-proxy.js
@@ -1,4 +1,9 @@
 const http = require('http');
+
+const fs = require("fs")
+const url = require("url")
+const path = require("path")
+
 const httpProxy = require('http-proxy');
 const auth = require('basic-auth');
 var Cookies = require('cookies');
@@ -7,9 +12,9 @@ var keys = ['keyboard cat'];
 
 // Create a proxy server with custom application logic
 const proxy = httpProxy.createProxyServer({changeOrigin: true, autoRewrite: true, hostRewrite: true, followRedirects: true});
-const normalwebsite = 'https://www.baidu.com';//默认值
-const defaulturl = 'https://www.google.com';// a default target
-var origin = normalwebsite;//默认值
+//const normalwebsite = 'https://www.baidu.com';//默认值
+//const defaulturl = 'https://www.google.com';// a default target
+var origin ;//默认值
 
 
 
@@ -21,7 +26,7 @@ const server = http.createServer(function(req, res) {
 //   const username = process.env.USERNAME;
   
   const password = '123456';//默认密码
-  const username = 'admin';//用户名是网址
+  const username = 'admin';//用户名
   
   
   const credentials = auth(req);
@@ -40,11 +45,13 @@ const server = http.createServer(function(req, res) {
 
 
   proxy.on('proxyRes', function(proxyRes, req, res) {
-    // console.log('Raw [target] response', JSON.stringify(proxyRes.headers, true, 2));
+    console.log('Raw [target] response', JSON.stringify(proxyRes.body, true, 2));
     
     proxyRes.headers['x-proxy'] = "simple-basic-http-auth-proxy-vercel";
         
     proxyRes.headers['x-proxy-domain'] = origin;
+    
+    
     
     
   });
@@ -53,13 +60,15 @@ const server = http.createServer(function(req, res) {
   var cookies = new Cookies(req, res, { keys: keys });
   if(req && req.url.substring(0,3).toUpperCase() == '/F/'){
     //更改目标
-     var targeturl = defaulturl;//默认url
+     var targeturl = '';//默认url
      var inurl = req.url.substring(3);
      
     if(inurl.substring(0,4).toUpperCase() == 'HTTP' ) {      
       //把丢失的/找回来
       inurl = inurl.replace('https:/','https://');
       inurl = inurl.replace('http:/','http://');
+      inurl = inurl.replace('https%3A/','https://');
+      inurl = inurl.replace('http%3A/','http://');
       targeturl = inurl;
       
     }
@@ -67,30 +76,63 @@ const server = http.createServer(function(req, res) {
      res.statusCode = 200;
       
       res.end('<!DOCTYPE html><html><head><script language="javascript" type="text/javascript">window.location.href="/";</script></head>cookie changed!</html>');
+    return;
   }
   
   if(req && req.url.substring(0,3).toUpperCase() == '/C/'){
-      origin = normalwebsite;//默认值  
+      //origin = normalwebsite;//默认值  
       cookies.set('lastorigin', '', { signed: true,maxAge:0 }); //删除 
       res.statusCode = 200;      
       res.end('<!DOCTYPE html><html><head><script language="javascript" type="text/javascript">window.location.href="/";</script></head>cookie clean!</html>');
+      return;
   }
   
   var lastorigin = cookies.get('lastorigin', { signed: true });
+  
     
   if(lastorigin && (lastorigin.indexOf('://') != -1)){
-    origin = lastorigin
+     console.log('show target........'+lastorigin); 
+     origin = lastorigin
   } 
  
-  if(typeof lastorigin == 'undefined'){
-   origin = normalwebsite;//默认值
+  if(typeof lastorigin == 'undefined' || lastorigin==''){
+    //origin = normalwebsite;//默认值
+    
+     console.log('show index.....'); 
+     res.statusCode = 200;      
+      const filePath = path.join(__dirname, 'g.dabeizi.com.html')
+    fs.readFile(filePath, "binary", (err, file) => {
+        if (err) {
+            res.writeHead(404, "not found")
+            res.end("<h1>404 NOT FOUND"+filePath+"</h1>")
+        } else {
+            res.write(file, "binary")
+            res.end()
+        }
+    })
+      
+    
+  }else{
+    console.log('now target================>',origin,lastorigin)
+    proxy.web(req, res, { target: `${origin}` });
   }
     
-  proxy.web(req, res, { target: `${origin}` });
+  
   
 });
 
-const port = process.env.AWS_LAMBDA_RUNTIME_API.split(':')[1];
+
+server.on('error', function (err, req, res) {
+    res.writeHead(500, {
+        'Content-Type': 'text/plain'
+    });
+
+    res.end('Something went wrong.');
+});
+
+
+
+const port = 3000;
 console.log(`simple-basic-http-auth-proxy for Vercel started on port ${port}`);
 server.listen(port);
 
@@ -99,3 +141,6 @@ const isAuthed = function (credentials, username, password) {
     return credentials.name === username && credentials.pass === password;
   
 }
+
+
+
